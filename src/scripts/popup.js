1503e878ece34b4fe60b95bae023d53f450f66bc
Mario Colque <colquemario@gmail.com>
2017-10-06 16:39:22 -0300

    improved UI

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,39 +1,52 @@
-import ext from "./utils/ext";
-import $ from "./vendor/jquery.min"
+var ext = require('./utils/ext');
+var $ = require('./vendor/jquery.min');
+var langs = require('./langs');
 
 $(document).on('ready', function () {
-  var lastLang = localStorage.getItem('lastLang');
+  var lastLangStr = localStorage.getItem('lastLang');
+  var lastLangObj = langs.find(function(lang) {
+    return lang.value === lastLangStr;
+  });
+
+  var $langSelector = $('#lang-selector');
+
+  langs.forEach(function(lang) {
+    $langSelector.append('<option value="' + lang.value + '">' + lang.label + '</option>');
+  });
 
   // Set the last lang selected
-  if (lastLang !== null) {
-    $('#lang-selector').val(lastLang);
+  if (lastLangStr !== null && lastLangStr !== 'plain_text') {
+    $langSelector
+      .prepend('<option value="-">------</option>')
+      .prepend('<option value="plain_text">Plain Text</option>')
+      .prepend('<option value="' + lastLangObj.value + '">' + lastLangObj.label + '</option>');
   }
 
-  $('#create-btn').on('click', function (e) {
-    e.preventDefault();
+  $langSelector
+    .prepend('<option value="">Select a lang...</option>')
+    .val('')
+    .on('change', function() {
+      $('#loading').show();
+      $langSelector.hide();
 
-    // Show loading spinner
-    $('#loading-text').show();
-    $('#create-text').hide();
+      // Get and save the selected lang
+      var lang = $langSelector.val();
+      if (lang != lastLangStr) {
+        localStorage.setItem('lastLang', lang);
+      }
 
-    // Get and save the selected lang
-    var lang = $('#lang-selector').val();
-    if (lang != lastLang) {
-      localStorage.setItem('lastLang', lang);
-    }
-
-    // Get the SCRAP data and open the new tab
-    $.ajax({
-      type: 'POST',
-      url: 'https://api.scrapfy.io/scraps',
-      contentType: 'application/json',
-      data: JSON.stringify({ lang: lang }),
-      dataType: 'json'
-    })
-      .done(function (data) {
+      // Get the SCRAP data and open the new tab
+      $.ajax({
+        type: 'POST',
+        url: 'https://api.scrapfy.io/scraps',
+        contentType: 'application/json',
+        data: JSON.stringify({ lang: lang }),
+        dataType: 'json'
+      })
+      .done(function(data) {
         ext.tabs.create({
           url: data.url
         });
       });
-  });
+    });
 });
